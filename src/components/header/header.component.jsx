e0b8b2a64c072fcdbb06e6c6fc3c4c954141ff63
Fabio Rodrigues <fabiordrig@gmail.com>
2020-06-10 19:50:15 -0300

    feat: integrate firebase

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.utils";
 import "./header.styles.scss";
 
-const Header = (props) => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo"></Logo>
@@ -11,6 +12,15 @@ const Header = (props) => (
     <div className="options">
       <Link className="option">SHOP</Link>
       <Link className="option">CONTACT</Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/login">
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
