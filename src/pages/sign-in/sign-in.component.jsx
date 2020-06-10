6c782cc3d95879c7ddcab0b71685698ab44e3bcb
Fabio Rodrigues <fabiordrig@gmail.com>
2020-06-10 20:47:09 -0300

    feat: adding store user data on app

--- src/pages/sign-in/sign-in.component.jsx
+++ src/pages/sign-in/sign-in.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./sign-in.styles.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInPage = (props) => (
-  <div className="sign-in">
+  <div className="login">
     <SignIn />
+    <SignUp />
   </div>
 );
 
