79f3f7580e709d60cb6958b4d68e399668e9239e
Fabio Rodrigues <fabiordrig@gmail.com>
2020-06-22 22:33:19 -0300

    feat: implemente new user reducer and action

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import store from "./redux/store";
 import { Provider } from "react-redux";
 import App from "./App";
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
